0a7709e4f05af62ff7a005890ba33bf6bbcc6e8f
sg200000 <saidguouihaj@gmail.com>
2023-02-19 11:30:08 +0000

    basic parse request

---
+++ inc/parse_request.h
@@ -0,0 +1,22 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+#define MAX_URI_LEN 128
+#define MAX_HOST_LEN    128
+#define MAX_VERSION_LEN 10
+#define MAX_METHOD_LEN  10
+#define MAX_USERAGENT_LEN   50
+#define MAX_ACCEPTLANG_LEN  20
+
+typedef struct {
+    char method[MAX_METHOD_LEN];
+    char version[MAX_VERSION_LEN];
+    char uri[MAX_URI_LEN];
+    char host[MAX_HOST_LEN];
+    char user_agent[MAX_USERAGENT_LEN];
+    char accept_language[MAX_ACCEPTLANG_LEN];
+    void * data;
+} fields_t;
+
+fields_t parse_request(char * frame, int length);
+#endif
---
+++ src/parse_request.c
@@ -0,0 +1,47 @@
+#include "../inc/parse_request.h"
+#include "../inc/debug.h"
+#include <string.h>
+#include <stdio.h>
+/*
+char req[1000] = "GET /hello.txt HTTP/1.1\nUser-Agent: curl/7.64.1\nHost: www.example.com\nAccept-Language: en, mi";
+
+int main(){
+    fields_t result = parse_request(req, 20);
+    printf("%s %s %s\n%s %s %s\n", result.method, result.uri, result.version, result.user_agent, result.host, result.accept_language); 
+    return 0;
+}
+*/
+fields_t parse_request(char * frame, int length){
+    fields_t usefulData;
+    int i = 0;
+    char * httpElement = strtok(frame, " ");
+    check(httpElement==NULL, "No method\n");
+    strncpy(usefulData.method, httpElement, MAX_METHOD_LEN);
+    httpElement = strtok(NULL, " ");
+    check(httpElement==NULL, "No uri\n");
+    strncpy(usefulData.uri, httpElement, MAX_URI_LEN);
+    httpElement = strtok(NULL, "\n");
+    check(httpElement==NULL, "No version\n");
+    strncpy(usefulData.version, httpElement, MAX_VERSION_LEN);
+    while ((httpElement = strtok(NULL, " "))!=NULL){
+        //httpElement = strtok(NULL, " ");
+        check(httpElement==NULL, "Frame error");
+        if (strcmp(httpElement, "Host:")==0){
+            httpElement = strtok(NULL, "\n");
+            strncpy(usefulData.host, httpElement, MAX_HOST_LEN);
+        }
+        else if (strcmp(httpElement, "User-Agent:")==0){
+            httpElement = strtok(NULL, "\n");
+            strncpy(usefulData.user_agent, httpElement, MAX_USERAGENT_LEN);
+        }
+        else if (strcmp(httpElement, "Accept-Language:")==0){
+            httpElement = strtok(NULL, "\n");
+            strncpy(usefulData.accept_language, httpElement, MAX_ACCEPTLANG_LEN);
+        }
+        else {
+            httpElement = strtok(NULL, "\n");
+        }
+    }
+    
+    return usefulData;
+}
